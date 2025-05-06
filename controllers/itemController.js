9f29618f31d094f6fb2a8b2c9a62a5ce9dfb8863
Bar Belity <barb@jfrog.com>
2025-05-06 13:45:07 +0300

    Update itemController.js

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -32,6 +32,15 @@ const search = async (req, res) => {
 	}
 }
 
+const vulnerableSearch = async (req, res) => {
+    try {
+        const result = await Item.find({ name: req.body.query })
+        return res.status(200).json({ message: 'success', result })
+	} catch (error) {
+		return res.status(500).json({ message: 'something went wrong!', error: error.stack })
+	}
+}
+
 module.exports = {
 	getItems,
 	addItem,
